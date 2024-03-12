ac21241997096307009a60858ba95f46aef83806
anjithajoby <154336678+anjithajoby@users.noreply.github.com>
2024-03-12 16:37:02 +0000

    Update BR1-3_105636.py

--- BR1-3_105636.py
+++ BR1-3_105636.py
@@ -8,21 +8,21 @@ from time import sleep
 #function to give a remark on my mood based on avg_mood value
 def interpret_mood(avg_mood):
     if avg_mood >= 9:
-        return "Excellent mood today, thank god"
+        return "i'm thrilled today, thankfully."
     elif avg_mood <  5:
-        return "Middlin today, thanks for asking"
+        return "i'm doing okay today, thanks for inquiring."
     elif 5 <= avg_mood < 9:
-        return "Improving thanks."
+        return "it's getting better, thanks.."
     else:
-        return "Not really sure, not enough info \n"
+        return "it's making progress, thanks\n"
 
 #Take them in as integers, as all inputs default to strings
-phone_anxietywellbeing= int(input("On a scale of 1-10, from slowly to quickly, how long does it take for you to fall asleep after using your phone? "))
-mental_wellbeing = int(input("On a scale of 1-10, from tired to energetic, how much energy do you have? "))
-sleep_wellbeing = int(input("On a scale of 1-10, from no sleep to loads of sleep, how do you feel about how much sleep you get ? "))
-avg_mood = round(mean([phone_anxietywellbeing,mental_wellbeing,sleep_wellbeing]),2)
+screentime_wellbeing = int(input("On a scale of 1-10, how do you feel about your screentime? "))
+mental_wellbeing = int(input("On a scale of 1-10 from tired to energetic, how much energy do you have? "))
+sleep_wellbeing = int(input("On a scale of 1-10, how do you feel about how much sleep you get ? "))
+avg_mood = round(mean([screentime_wellbeing,mental_wellbeing,sleep_wellbeing]),2)
 mood_remark = interpret_mood(avg_mood)
-print("My Average mood today is ",mood_remark, " ", avg_mood)
+print("My average mood today is that",mood_remark, " ", avg_mood)
 
 df = pd.read_csv('BR1-3_105636.csv')
 print(df)
