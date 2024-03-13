42af3f2bcb463ece5c68af57753720473305ec76
anjithajoby <154336678+anjithajoby@users.noreply.github.com>
2024-03-13 06:02:49 +0000

    Update AR1-3_105636.py

--- AR1-3_105636.py
+++ AR1-3_105636.py
@@ -40,12 +40,12 @@ mse = mean_squared_error(Y_test, Y_pred)
  # Let the user enter their own 3 parameters
 print("")
 print("User will be asked three questions!")
-bluelight = int(input("Enter the amount of blue light you receive from your phone.It can be any integer from 0-300. "))
-mental = float(input("Enter how much time you spend to yourself. Can be anything from 0-24 "))
-sleeptime = float(input("Enter how much sleep you get in a day. Can be anything from 0-24 "))
+phoneanxiety_wellbeing = int(input("Enter the amount of blue light you receive from your phone.It can be any integer from 0-300. "))
+mental_wellbeing = float(input("Enter how much time you spend to yourself. Can be anything from 0-24 "))
+sleep_wellbeing = float(input("Enter how much sleep you get in a day. Can be anything from 0-24 "))
 
-predicted_mood = predict_mood(bluelight, mental, sleeptime)  # Example values
-print("\n The predicted mood score for the values entered is", predicted_mood)
+predicted_mood = round(predict_mood(phoneanxiety_wellbeing, mental_wellbeing, sleep_wellbeing)) # Example values
+print("\n Your mood score for the values you entered are", predicted_mood)
 
 #WHAT-IF Question 1
 # What is will your mood be with low values of all three parameters?
@@ -53,34 +53,51 @@ print("-----------------------------------------------------------")
 print("What-if Question 1")
 print("Let's test how much blue light the user receives from their phone ")
 # Low values for all 3 parameters
-
-mood_if_littlelight = predict_mood(bluelight, mental, sleeptime)  # Example values
-print("\n The amount of blue light the user receives is ", mood_if_littlelight)
-
+bluelight = predict_mood(phoneanxiety_wellbeing, mental_wellbeing, sleep_wellbeing)  # Example values
+print("\n The amount of blue light the user receives is ", phonelight)
+def interpret_mood(bluelight):
+    if bluelight >= 9:
+        return "This is too much blue light exposure. Put that phone down right now, your snaps can wait."
+    elif 5 <= bluelight < 9:
+        return "This isn't too bad. Why don't you read a book? You will have less strain on your eyes."
+    else:
+        return "This is better. Try and go for longer periods without your phone"
+phonelight = interpret_mood(phoneanxiety_wellbeing)
 # WHAT-IF Question 2
 # What is will your mood be with high values of all three parameters?
 print("-----------------------------------------------------------")
 print("What-if Question 2")
 print("Let's test how much time the user spends to theirself")
 # High values for all 3 parameters
-
-mood_if_Loadsalight = predict_mood(bluelight, mental, sleeptime)  # Example values
-print("\n The amount of time the user spends to theirself is ", mood_if_Loadsalight)
-
+mental = predict_mood(phoneanxiety_wellbeing, mental_wellbeing, sleep_wellbeing)  # Example values
+print("\n The amount of time the user spends to theirself is ", mentalhealth)
+def interpret_mood(mental):
+    if bluelight >= 9:
+        return "This is brilliant. Spending time with yourself can improve your mental health."
+    elif 5 <= bluelight < 9:
+        return "Okay you can do better. Engage in activities that bring you relaxation. It's important to give yourself a break."
+    else:
+        return "No this is not good. Try and just spend a few minutes to yourself. You will see huge improvements in your mental health."
+mentalhealth = interpret_mood(mental_wellbeing)
 # WHAT IF QUESTION 3
 # What variable is more important
 print("-----------------------------------------------------------")
 print("What-if Question 3")
 print("Let's test how much sleep the user gets in a day")
-print("We will keep the hours (A) the same and double the others (B) and (C) one at a time")
-print("")
-print("Let's get a baseline from fairly average values...")
+sleep = predict_mood(phoneanxiety_wellbeing, mental_wellbeing, sleep_wellbeing)  # Example values
+print("\n The score mood is", round(sleeptime))
+def interpret_mood(mental):
+    if bluelight >= 9:
+        return "Keep this up. Sleeping is good for you, and you won't have eyebags if you sleep this long."
+    elif 5 <= bluelight < 9:
+        return "Okay this isn't bad. Make sure you go to bed on time and don't be on your phone before you sleep"
+    else:
+        return "Please tell me your joking. If it's because of the phone you don't sleep well, then leave your phone in another room before you get ready to go to sleep"
+sleeptime = interpret_mood(sleep_wellbeing)
+
+
 
 # Baseline 
-
-
-baseline_mood = predict_mood(bluelight, mental, sleeptime)  # Example values
-print("\n The score mood is", round(baseline_mood))
 print("")
 
 
@@ -101,4 +118,3 @@ plt.title('Bar Chart of What-if Q1, Q2, Q3 Outcomes')
 
 # Show the plot
 plt.show()
-
